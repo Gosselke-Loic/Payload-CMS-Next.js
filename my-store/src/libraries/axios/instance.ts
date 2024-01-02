23098ce3eb608a2a367b2d5dd92f551886755251
Gosselke Loic <99717440+Gosselke-Loic@users.noreply.github.com>
2024-01-02 11:28:06 +0100

    Update instance.ts

--- my-store/src/libraries/axios/instance.ts
+++ my-store/src/libraries/axios/instance.ts
@@ -4,10 +4,6 @@ const init = (config: AxiosRequestConfig): AxiosInstance => {
     const axiosInstance = axios.create(config);
 
     axiosInstance.interceptors.request.use(function (config) {
-        /*  if(!!token) {
-            config.headers = { "Authorization": `Bearer ${token}` };
-            if(!!currentTenant) config.headers = { "X-Tenant": `${currentTenant}` };
-        } */
         return config;
     }, function (error) {
         return Promise.reject(error);
